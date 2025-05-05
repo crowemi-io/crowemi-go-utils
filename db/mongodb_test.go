3c00c73a02f2e99a0c1066b2d7cb52132d5b0142
Andy Crowe <crowemi@hotmail.com>
2025-05-04 20:08:07 -0700

    update tests

--- db/mongodb_test.go
+++ db/mongodb_test.go
@@ -3,6 +3,7 @@ package db
 import (
 	"context"
 	"testing"
+	"time"
 
 	"github.com/crowemi-io/crowemi-go-utils/config"
 )
@@ -70,65 +71,65 @@ func TestMongoDBInsertOne(t *testing.T) {
 	}
 }
 
-// func TestMongoDBInsertMany(t *testing.T) {
-// 	c := setup()
-// 	type symbol struct {
-// 		Symbol string `bson:"symbol"`
-// 	}
-// 	s := []symbol{
-// 		{Symbol: "OXY1"},
-// 		{Symbol: "AAPL1"},
-// 	}
-// 	ret, err := InsertMany(context.TODO(), *c, "symbol", s)
-// 	if err != nil {
-// 		t.Errorf("Failed to insert many documents: %v", err)
-// 	}
-// 	if ret == nil {
-// 		t.Errorf("Expected to get an ID, got nil")
-// 	} else if len(ret.InsertedIDs) == 0 {
-// 		t.Errorf("Expected to insert one document, got %d", ret.InsertedIDs...)
-// 	}
-// }
-// func TestMongoDBUpdateOne(t *testing.T) {
-// 	c := setup()
-// 	type symbol struct {
-// 		Symbol    string    `bson:"symbol"`
-// 		CreatedAt time.Time `bson:"created_at"`
-// 	}
-// 	s := symbol{Symbol: "OXY", CreatedAt: time.Now()}
-// 	f := map[string]string{"symbol": "OXY"}
-// 	ret, err := UpdateOne(context.TODO(), *c, "symbol", f, s)
-// 	if err != nil {
-// 		t.Errorf("Failed to update one document: %v", err)
-// 	}
-// 	if ret == nil {
-// 		t.Errorf("Expected to get an ID, got nil")
-// 	}
-// }
-// func TestMongoDBUpdateMany(t *testing.T) {}
-// func TestMongoDBDeleteOne(t *testing.T) {
-// 	c := setup()
-// 	f := map[string]string{"symbol": "OXY"}
-// 	ret, err := DeleteOne(context.TODO(), *c, "symbol", f)
-// 	if err != nil {
-// 		t.Errorf("Failed to delete one document: %v", err)
-// 	}
-// 	if ret == nil {
-// 		t.Errorf("Expected to get an ID, got nil")
-// 	} else if ret.DeletedCount == 0 {
-// 		t.Errorf("Expected to delete one document, got %d", ret.DeletedCount)
-// 	}
-// }
-// func TestMongoDBDeleteMany(t *testing.T) {
-// 	c := setup()
-// 	f := map[string]string{"symbol": "OXY1"}
-// 	ret, err := DeleteMany(context.TODO(), *c, "symbol", f)
-// 	if err != nil {
-// 		t.Errorf("Failed to delete many documents: %v", err)
-// 	}
-// 	if ret == nil {
-// 		t.Errorf("Expected to get an ID, got nil")
-// 	} else if ret.DeletedCount == 0 {
-// 		t.Errorf("Expected to delete one document, got %d", ret.DeletedCount)
-// 	}
-// }
+func TestMongoDBInsertMany(t *testing.T) {
+	c := setup()
+	type symbol struct {
+		Symbol string `bson:"symbol"`
+	}
+	s := []symbol{
+		{Symbol: "OXY1"},
+		{Symbol: "AAPL1"},
+	}
+	ret, err := InsertMany(context.TODO(), *c, "symbol", s)
+	if err != nil {
+		t.Errorf("Failed to insert many documents: %v", err)
+	}
+	if ret == nil {
+		t.Errorf("Expected to get an ID, got nil")
+	} else if len(ret.InsertedIDs) == 0 {
+		t.Errorf("Expected to insert one document, got %d", ret.InsertedIDs...)
+	}
+}
+func TestMongoDBUpdateOne(t *testing.T) {
+	c := setup()
+	type symbol struct {
+		Symbol    string    `bson:"symbol"`
+		CreatedAt time.Time `bson:"created_at"`
+	}
+	s := symbol{Symbol: "OXY", CreatedAt: time.Now()}
+	f := []MongoFilter{{Field: "symbol", Operator: "$eq", Value: "OXY"}}
+	ret, err := UpdateOne(context.TODO(), *c, "symbol", f, s)
+	if err != nil {
+		t.Errorf("Failed to update one document: %v", err)
+	}
+	if ret == nil {
+		t.Errorf("Expected to get an ID, got nil")
+	}
+}
+func TestMongoDBUpdateMany(t *testing.T) {}
+func TestMongoDBDeleteOne(t *testing.T) {
+	c := setup()
+	f := []MongoFilter{{Field: "symbol", Operator: "$eq", Value: "OXY1"}}
+	ret, err := DeleteOne(context.TODO(), *c, "symbol", f)
+	if err != nil {
+		t.Errorf("Failed to delete one document: %v", err)
+	}
+	if ret == nil {
+		t.Errorf("Expected to get an ID, got nil")
+	} else if ret.DeletedCount == 0 {
+		t.Errorf("Expected to delete one document, got %d", ret.DeletedCount)
+	}
+}
+func TestMongoDBDeleteMany(t *testing.T) {
+	c := setup()
+	f := []MongoFilter{{Field: "symbol", Operator: "$eq", Value: "AAPL1"}}
+	ret, err := DeleteMany(context.TODO(), *c, "symbol", f)
+	if err != nil {
+		t.Errorf("Failed to delete many documents: %v", err)
+	}
+	if ret == nil {
+		t.Errorf("Expected to get an ID, got nil")
+	} else if ret.DeletedCount == 0 {
+		t.Errorf("Expected to delete one document, got %d", ret.DeletedCount)
+	}
+}
