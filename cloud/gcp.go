7718cd376a6c192c05baa55169726363cc01bf62
Andy Crowe <crowemi@hotmail.com>
2025-05-14 07:22:14 -0700

    chore: refactor logging; gcp client

--- cloud/gcp.go
+++ cloud/gcp.go
@@ -1,11 +1,35 @@
 package cloud
 
-import "github.com/crowemi-io/crowemi-go-utils/config"
+import (
+	"context"
+
+	"cloud.google.com/go/storage"
+	"github.com/crowemi-io/crowemi-go-utils/config"
+)
 
 type GcpClient struct {
-	App     string
-	Session string
-	Config  *config.GoogleCloud
+	App    string
+	Config *config.GoogleCloud
 	// Add GCP components
 	CloudStorage *CloudStorage
+}
+
+func NewGcpClient(app string, config *config.GoogleCloud) (*GcpClient, error) {
+	storageClient, err := storage.NewClient(context.TODO())
+	if err != nil {
+		return nil, err
+	}
+
+	bucket := storageClient.Bucket(app)
+
+	cloudStorage := CloudStorage{
+		Bucket:        bucket,
+		StorageClient: storageClient,
+	}
+	client := GcpClient{
+		App:          app,
+		Config:       config,
+		CloudStorage: &cloudStorage,
+	}
+	return &client, nil
 }
