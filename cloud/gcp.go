96bdee6456c07b41f319e80cf40eee8883c5cc4d
Andy Crowe <crowemi@hotmail.com>
2025-05-14 06:54:21 -0700

    refactor gcp client

--- cloud/gcp.go
+++ cloud/gcp.go
@@ -3,8 +3,9 @@ package cloud
 import "github.com/crowemi-io/crowemi-go-utils/config"
 
 type GcpClient struct {
-	App           string
-	Session       string
-	Config        *config.GoogleCloud
-	StorageClient *GcpStorageClient
+	App     string
+	Session string
+	Config  *config.GoogleCloud
+	// Add GCP components
+	CloudStorage *CloudStorage
 }
