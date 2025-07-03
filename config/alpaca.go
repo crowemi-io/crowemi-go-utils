fe98560bcd02ed8fbfd566f68b110874be90fb9b
Andy Crowe <crowemi@hotmail.com>
2025-07-03 07:33:03 -0700

    update account_id

--- config/alpaca.go
+++ config/alpaca.go
@@ -1,7 +1,7 @@
 package config
 
 type Alpaca struct {
-	ID           string `json:"id" omitempty:"true"`
+	AccountID    string `json:"account_id" omitempty:"true"`
 	APIKey       string `json:"api_key" omitempty:"true"`
 	APISecretKey string `json:"api_secret_key" omitempty:"true"`
 	APIBaseURL   string `json:"api_base_url" omitempty:"true"`
